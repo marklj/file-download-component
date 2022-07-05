ee4a263ff039b5024e5a24fe130d6747d61682a2
Mark Johnson <marklj@gmail.com>
2022-07-05 09:25:23 -0700

    add download list component scaffolding

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,12 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { DownloadListComponent } from './download-list/download-list.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    DownloadListComponent
   ],
   imports: [
     BrowserModule
---
+++ src/app/download-list/download-list.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-download-list',
+  templateUrl: './download-list.component.html',
+  styleUrls: ['./download-list.component.css']
+})
+export class DownloadListComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
