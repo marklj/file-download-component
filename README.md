fc28d3afa24d73f918de4a5e7df2e399f634a501
Mark Johnson <marklj@gmail.com>
2022-07-06 08:58:58 -0700

    remove mention of anything CS related

--- README.md
+++ README.md
@@ -1,4 +1,4 @@
-# CrowdstrikeComponent
+# File DownloadComponent
 
 This project was generated with [Angular CLI](https://github.com/angular/angular-cli) version 14.0.4.
 
--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -27,7 +27,7 @@ describe('AppComponent', () => {
     fixture.detectChanges();
     const compiled = fixture.nativeElement as HTMLElement;
     expect(compiled.querySelector('h1')?.textContent).toContain(
-      'Crowdstrike UI Developer Test'
+      'File Download UI Developer Test'
     );
   });
 });
